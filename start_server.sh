b32cc0c8497c2e9cebd542c77e5e7d1b1a5c2b59
Brian Dorton <dorton@gmail.com>
2023-11-14 20:18:47 -0600

    server script tweaks

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 RAILS_ENV=production rails db:migrate &&
 RAILS_ENV=production rails assets:precompile && 
+bundle exec sidekiq -e production -d &
 rails s -b 192.168.86.173 -p 3004 -e production -d
