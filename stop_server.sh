a96d0549a1dbd36c11caeecac45f944470cbeaa4
Brian Dorton <dorton@gmail.com>
2023-11-16 09:50:18 -0600

    remove stopping sidkiq from script

--- stop_server.sh
+++ stop_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-kill `ps aux | grep sidekiq | grep -v grep | awk '{print $2}'` &&
+# kill `ps aux | grep sidekiq | grep -v grep | awk '{print $2}'` &&
 lsof -t -i:3004 | xargs kill -9
